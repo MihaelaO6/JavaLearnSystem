9d05ac36188252328c57c8fb46c90fc5d0c437a5
Mihaela Obadikj <obadikm1408@gmail.com>
2022-09-04 11:36:39 +0200

    Commit

--- JavaLearnSystem/urls.py
+++ JavaLearnSystem/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from testapp.views import index,blog,materials, quizz, reminder, sertificate, login, signup
+from django.conf import settings
+from django.conf.urls.static import static
 
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,5 +32,8 @@ urlpatterns = [
     path('sertificate/', sertificate, name="sertificate"),
     path('login/', login, name="login"),
     path('signup/', signup, name="signup"),
+
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 
 ]
